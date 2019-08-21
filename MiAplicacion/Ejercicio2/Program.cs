a3f61a2ae647f1ef5bfc33d5efdfe038e41a3ac6
alumno <alumno@utnfra.com.ar>
2019-08-20 21:02:23 -0300

    Resolviendo ejercicios c#

---
+++ MiAplicacion/Ejercicio2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string variable;
+            int res;
+            double cuadrado;
+            double cubo;
+            Console.Title = "Ejercicio Nro 2";
+            do
+            {
+                Console.Write("Ingrese un numero mayor que cero: ");
+                variable = Console.ReadLine();
+                if (int.TryParse(variable, out res))
+                {
+                    if (res < 0)
+                    {
+                        Console.WriteLine("ERROR! Reingresar numero");
+                    }
+                }
+            } while (res < 0);
+
+            cuadrado = Math.Pow(res, 2);
+            cubo = Math.Pow(res, 3);
+
+            Console.WriteLine("El cuadrado es : {0}", cuadrado);
+            Console.WriteLine("El cubo es : {0}", cubo);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MiAplicacion/MiAplicacion/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiAplicacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro 1";
+            string variable;
+            int res;
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            int i;
+            int sumaTotal = 0;
+            float promedio;
+            int contador = 0;
+
+            for(i= 0;i< 4 ;i++ )
+            {
+                Console.Write("Ingrese un numero, para terminar ingrese 0: ");
+                variable = Console.ReadLine();
+                if (int.TryParse(variable, out res))
+                {
+                        
+                    if (res > max)
+                        max = res;
+
+                    if (res < min)
+                        min = res;
+
+                    sumaTotal += res;
+                    contador++;
+                }
+            }
+
+            promedio = (float)sumaTotal / contador;
+            Console.WriteLine("El promedio es : {0}", promedio);
+            Console.WriteLine("El maximo es : {0}", max);
+            Console.WriteLine("El minimo es : {0}", min);
+
+            Console.ReadKey();
+
+        }
+    }
+}
