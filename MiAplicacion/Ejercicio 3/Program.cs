5aa39edee67d3fd318cad826c0fb81269c45105a
alumno <alumno@utnfra.com.ar>
2019-08-20 22:12:13 -0300

    Resolviendo ejercicios c#

--- MiAplicacion/Ejercicio 3/Program.cs
+++ MiAplicacion/Ejercicio 3/Program.cs
@@ -10,7 +10,38 @@ namespace Ejercicio_3
     {
         static void Main(string[] args)
         {
+            Console.Title = "Ejercicio nro 3";
+            string variable;
+            int res;
+            int i, j;
+            //int modulo;
+            int contador = 0;
 
+            do
+            {
+                Console.Write("Ingrese un numero: ");
+                variable = Console.ReadLine();
+                if (int.TryParse(variable, out res))
+                {
+                    if (res < 0)
+                    {
+                        Console.WriteLine("ERROR! Reingresar numero");
+                    }
+                }
+            } while (res < 0);
+
+            for(i = 2; i < res; i++)
+            {
+                contador = 0;
+                for(j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                        contador++;                               
+                }
+                if (contador == 1)
+                    Console.WriteLine("{0}", i);
+            }
+            Console.ReadKey();
         }
     }
 }
