b8e9116c1f2d9ee10675f20b2c98f23f2ffd884e
FabianRolon <fabi18fr@gmail.com>
2019-08-25 22:53:58 -0300

    ejercicio 5 falta completar

--- MiAplicacion/Ejercicio 5/Program.cs
+++ MiAplicacion/Ejercicio 5/Program.cs
@@ -10,7 +10,28 @@ namespace Ejercicio_5
     {
         static void Main(string[] args)
         {
+            Console.Title = "Ejercicio nro 5";
+            Console.ForegroundColor = ConsoleColor.Red;
+            string variable;
+            int aux;
+          //  int i, j;
+          //  int contador = 0;
 
+            do
+            {
+                Console.Write("Ingrese un numero: ");
+                variable = Console.ReadLine();
+                if (int.TryParse(variable, out aux))
+                {
+                    if (aux < 0)
+                    {
+                        Console.WriteLine("ERROR! Reingresar numero");
+                    }
+                }
+            } while (aux < 0);
+
+            
+            Console.ReadKey();
         }
     }
 }
