f52ad038d868285ef452e5b6c98ab1185b4a8f31
alumno <alumno@utnfra.com.ar>
2019-08-27 22:06:54 -0300

    Empezando ej 16

---
+++ Clase_03/Clase_03/Alumno.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_03
+{
+    class Alumno
+    {
+        int nota1;
+        int nota2;
+        double notaFinal;
+        string apellido;
+        int legajo;
+        string nombre;
+           
+        public Alumno(string apellido, int legajo, string nombre)
+        {
+            this.apellido = apellido;
+            this.legajo = legajo;
+            this.nombre = nombre;
+        }
+    }
+}
---
+++ Clase_03/Clase_03/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno alumno = new Alumno("Rolon", 21, "Fabian");
+
+        }
+    }
+}
