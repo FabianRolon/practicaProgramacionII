b480d466a5fbfba883825e5943c1c31d8adb4830
FabianRolon <fabi18fr@gmail.com>
2019-09-04 17:57:25 -0300

    ejercicio 20, comenzando

---
+++ Ejercicio_20/Pesos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_20
+{
+    class Pesos
+    {
+        double cantidad;
+        static double cotizRespectoDolar = 55.93;
+
+        Pesos(){}
+        Pesos(double cantidad)
+        {
+            this.cantidad = cantidad;
+        }
+
+        Pesos(double cantidad, double cotizacion) 
+            : this(cantidad)
+        {
+            Pesos.cotizRespectoDolar = cotizacion;
+        }
+        
+        
+    }
+}
