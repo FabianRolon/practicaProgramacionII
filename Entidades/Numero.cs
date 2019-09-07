7ea77a36694fa5a72d5b8224670a1f1dbd737886
FabianRolon <fabi18fr@gmail.com>
2019-09-07 16:46:17 -0300

    esto no es coca papi

---
+++ Entidades/Numero.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class Numero
+    {
+        double numero;
+
+        public Numero()
+        {
+            numero = 0;
+        }
+
+        public Numero(double numero)
+        {
+            this.numero =  numero;
+        }
+
+        public Numero(string strNumero)
+        {
+            
+        }
+
+        private double ValidarNumero(string strNumero)
+        {
+            double auxNumero;
+            if (double.TryParse(strNumero, out auxNumero))
+            {
+                return auxNumero;
+            }
+            else
+                return 0;
+        }
+
+        public void SetNumero(string strNumero)
+        {
+            this.numero = ValidarNumero(strNumero);
+        }
+    }
+}
