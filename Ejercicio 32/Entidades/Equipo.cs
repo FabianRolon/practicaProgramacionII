f68f25567a1deb68aa5da5df1d002c05ba67e781
FabianRolon <fabi18fr@gmail.com>
2020-04-17 00:20:02 -0300

    ejercicios del dia

---
+++ Ejercicio 32/Entidades/Equipo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Equipo
+    {
+        short cantidadDeJugadores;
+        List<Jugador> jugadores;
+        string nombre;
+
+        private Equipo()
+        {
+            jugadores = new List<Jugador>();
+        }
+
+        public Equipo(short cantidadDeJugadores, string nombre)
+        {
+            this.cantidadDeJugadores = cantidadDeJugadores;
+            this.nombre = nombre;
+        }
+
+        public static bool operator +(Equipo e, Jugador j)
+        {
+            bool flag = true;
+            if (e.jugadores.Count() < e.cantidadDeJugadores)
+            {
+                foreach (Jugador jugador in e.jugadores)
+                {
+                    if (jugador == j)
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+                e.jugadores.Add(j);
+            }
+            else
+            {
+                flag = false;
+            }
+            return flag;           
+        }
+    }
+}
---
+++ Ejercicio29/Entidades/Jugador.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Jugador
+    {
+        int dni;
+        string nombre;
+        int partidosJugados;
+        float promedioGoles;
+        int totalGoles;
+
+        private Jugador()
+        {
+            partidosJugados = 0;
+            promedioGoles = 0;
+            totalGoles = 0;
+        }
+        public Jugador(int dni, string nombre)
+            :this()
+        {
+            this.dni = dni;
+            this.nombre = nombre;
+        }
+
+        public Jugador(int dni, string nombre, int partidosJugados, int promedioGoles, int totalGoles)
+            :this(dni, nombre)
+        {
+            this.partidosJugados = partidosJugados;
+            this.promedioGoles = GetPromediogoles();
+            this.totalGoles = totalGoles;
+        }
+
+        public float GetPromediogoles()
+        {
+            return this.totalGoles / (float)this.partidosJugados;
+        }
+
+        public string MostrarDatos()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("Nombre: {0}\nDNI: {1}\nPartidos Jugados: {2}\nPromedio de goles: {3}\nTotal de goles: {4}\n", nombre, dni, partidosJugados, GetPromediogoles(), totalGoles);
+            return sb.ToString();
+        }
+
+        public static bool operator ==(Jugador j1, Jugador j2)
+        {
+            if (j1.dni == j2.dni)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator !=(Jugador j1, Jugador j2)
+        {
+            return !(j1 == j2);
+        }
+    }
+}
