a7f4de17dd643f871a0b0c45c0107e8ccd9bb992
FabianRolon <fabi18fr@gmail.com>
2020-04-22 22:51:59 -0300

    Centralita Ej37 TERMINADO

---
+++ CentralitaEj37/CentralitaEj37/Centralita.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralitaEj37
+{
+    public class Centralita
+    {
+        private List<Llamada> listaDeLLamadas;
+        protected string razonSocial;
+
+        public Centralita()
+        {
+            listaDeLLamadas = new List<Llamada>();
+        }
+
+        public Centralita(string nombreDeLaEmpresa)
+            :this()
+        {
+            this.razonSocial = nombreDeLaEmpresa;
+        }
+
+        public float GananciasPorLocal
+        {
+            get
+            {
+                return CalcularGanancia(TipoLlamda.Local);
+            }
+        }
+
+        public float GananciasPorProvincial
+        {
+            get
+            {
+                return CalcularGanancia(TipoLlamda.Provincial);
+            }
+        }
+
+        public float GananciasPortotal
+        {
+            get
+            {
+                return CalcularGanancia(TipoLlamda.Todas);
+            }
+        }
+
+        public List<Llamada> Llamadas
+        {
+            get
+            {
+                return this.listaDeLLamadas;
+            }
+        }
+
+        private float CalcularGanancia(TipoLlamda tipo)
+        {
+            float totalLocal = 0;
+            float totalProvincial = 0;
+
+            foreach (Llamada llamada in this.Llamadas)
+            {
+                if (llamada is Local)
+                    totalLocal += ((Local)llamada).CostoLlamada;
+                if (llamada is Provincial)
+                    totalProvincial += ((Provincial)llamada).CostoLlamada;
+            }
+            
+            switch (tipo)
+            {
+                case TipoLlamda.Local:
+                    return totalLocal;
+                case TipoLlamda.Provincial:
+                    return totalProvincial;
+                case TipoLlamda.Todas:
+                    return totalLocal + totalProvincial;
+                default:
+                    return 0;
+            }
+        }
+
+        public string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Razon Social: {this.razonSocial}");
+            sb.AppendLine($"Ganancia Total: {this.GananciasPortotal}");
+            sb.AppendLine($"Ganancia Local: {this.GananciasPorLocal}");
+            sb.AppendLine($"Ganancia Provincial: {this.GananciasPorProvincial}");
+            sb.AppendLine($"Detalle de llamada:");
+            foreach (Llamada llamada in Llamadas)
+            {
+                sb.AppendLine($"{llamada.Mostrar()}");
+            }
+            return sb.ToString();
+        }
+
+        public void OrdenarLlamadas()
+        {
+            Llamadas.Sort(Llamada.OrdenarPorDuracion);
+        }
+    }
+}
--- CentralitaEj37/ConsolaCentralita/Properties/AssemblyInfo.cs
+++ CentralitaEj37/ConsolaCentralita/Properties/AssemblyInfo.cs
@@ -4,13 +4,13 @@ using System.Runtime.InteropServices;
 
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
-// asociada con un ensamblado.
-[assembly: AssemblyTitle("CentralitaHerencia")]
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("ConsolaCentralita")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("CentralitaHerencia")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsolaCentralita")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("66c2063c-fd69-41fe-a878-cdb2dfcb41e6")]
+[assembly: Guid("068a6a15-7e98-44cb-b285-0eb50417abd5")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
@@ -29,8 +29,8 @@ using System.Runtime.InteropServices;
 //      Número de compilación
 //      Revisión
 //
-// Puede especificar todos los valores o utilizar los números de compilación y de revisión predeterminados
-// mediante el carácter '*', como se muestra a continuación:
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
