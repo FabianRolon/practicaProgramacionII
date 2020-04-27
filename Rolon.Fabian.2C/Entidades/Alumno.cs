d2af6f4e7ad30b129a79f768b5efce27a7fb64b2
FabianRolon <fabi18fr@gmail.com>
2020-04-27 17:45:10 -0300

    parcial viejo 2018

---
+++ Rolon.Fabian.2C/Entidades/Alumno.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Alumno : Persona
+    {
+        private short anio;
+        Divisiones division;
+
+        public Alumno(string nombre, string apellido, string documento, short anio, Divisiones division)
+            :base(apellido, documento, nombre)
+        {
+            this.anio = anio;
+            this.division = division;
+        }
+
+        public string AnioDivision
+        {
+            get
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine($"{this.anio}º{this.division}");
+                return sb.ToString();
+            }
+        }
+
+        public override string ExponerDatos()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{base.ExponerDatos()}");
+            sb.AppendLine($"{this.AnioDivision}");
+            return sb.ToString();
+        }
+
+        public override bool ValidarDocumentacion(string doc)
+        {
+            doc.Contains("xx-xxxx-x");
+        }
+    }
+}
---
+++ Rolon.Fabian.2C/Entidades/Persona.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public abstract class Persona
+    {
+        private string apellido;
+        private string documento;
+        private string nombre;
+
+        protected Persona(string apellido, string documento, string nombre)
+        {
+            this.apellido = apellido;
+            this.documento = documento;
+            this.nombre = nombre;
+        }
+
+        public string Apellido
+        {
+            get
+            {
+                return this.apellido;
+            }
+        }
+
+        public string Documento
+        {
+            get
+            {
+                return this.documento;
+            }
+            set
+            {
+                this.documento = value;
+            }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return this.nombre;
+            }
+        }
+
+        public virtual string ExponerDatos()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Nombre: {this.Nombre}");
+            sb.AppendLine($"Apellido: {this.Apellido}");
+            sb.AppendLine($"Dni: {this.Documento}");
+            return sb.ToString();
+        }
+
+        protected abstract bool ValidarDocumentacion(string doc);
+
+    }
+}
