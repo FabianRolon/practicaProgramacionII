6e6f46bee9343bcc87b4506e1498964b9e855aba
FabianRolon <fabi18fr@gmail.com>
2020-04-28 17:47:19 -0300

    Terminando Clase Grupo

---
+++ Rolon.Fabian.2C/Entidades/Curso.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Curso
+    {
+        private List<Alumno> alumnos;
+        private short anio;
+        Divisiones division;
+        Profesor profesor;
+
+        private Curso()
+        {
+            this.alumnos = new List<Alumno>();
+        }
+
+        public Curso(short anio, Divisiones division, Profesor profesor)
+        {
+            this.anio = anio;
+            this.division = division;
+            this.profesor = profesor;
+        }
+
+        public string AnioDivision
+        {
+            get
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine($"{this.anio}º{this.division}");
+                return sb.ToString();
+            }
+        }
+
+        public static explicit operator string(Curso c)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Curso: {c.AnioDivision}");
+            sb.AppendLine($"Profesor: {c.profesor}");
+            sb.AppendLine($"Alumnos:");
+            foreach (Alumno alumno in c.alumnos)
+            {
+                sb.AppendLine($"{alumno.ExponerDatos()}");
+            }
+            return sb.ToString();
+        }
+
+        public implicit bool operator ==(Curso c, Alumno a)
+    }
+}
---
+++ Rolon.Fabian.2C/Entidades/Profesor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Profesor : Persona
+    {
+        DateTime fechaDeIngreso;
+
+        public Profesor(string nombre, string apellido, string documento)
+            :base(apellido, documento, nombre){}
+
+        public Profesor(string nombre, string apellido, string documento, DateTime fecha)
+            :this(nombre, apellido, documento)
+        {
+            this.fechaDeIngreso = fecha;
+        }
+
+        public override string ExponerDatos()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{base.ExponerDatos()}");
+            sb.AppendLine($"{this.fechaDeIngreso}");
+            return sb.ToString();
+        }
+
+        protected override bool ValidarDocumentacion(string doc)
+        {
+            bool retorno = false;
+            if (doc.Length == 9)
+            {
+                foreach (char c in doc)
+                {
+                    if (!(char.IsDigit(c)))
+                    {
+                        return retorno;
+                    }
+                }
+                retorno = true;
+            }
+            return retorno;
+        }
+    }
+}
