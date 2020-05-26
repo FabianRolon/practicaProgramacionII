c9f9ad4e437ba2652c4b54c03a40e8734d3bc926
FabianRolon <fabi18fr@gmail.com>
2020-05-25 22:23:32 -0300

    Ejercicio 48

---
+++ Ejercicio48/Ejercicio48/Recibo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio48
+{
+    public class Recibo : Documento
+    {
+        public Recibo()
+            :this(new int())
+        {
+
+        }
+        public Recibo(int numero)
+            :base(numero)
+        {
+
+        }
+
+    }
+}
---
+++ Ejercicio48/VistaConsola/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ejercicio48;
+
+namespace VistaConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
