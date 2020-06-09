5b6f158874e5cd5d30c502a01fd3189a59cd2ecd
FabianRolon <fabi18fr@gmail.com>
2020-06-09 17:46:58 -0300

    Ej61

---
+++ Ejercicio61/Ejercicio61/PErsona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio61
+{
+    public class Persona
+    {
+        private int id;
+        private string nombre;
+        private string apellido;
+
+        public Persona(int id, string nombre, string apellido)
+            :this(nombre, apellido)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.apellido = apellido;
+        }
+        public Persona(string nombre, string apellido)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+        }
+    }
+}
