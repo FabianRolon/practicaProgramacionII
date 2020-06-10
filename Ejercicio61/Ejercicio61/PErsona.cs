fb7f6487aca711a82606b76843f3bae4323b0da5
FabianRolon <fabi18fr@gmail.com>
2020-06-10 17:43:08 -0300

    Ej 61 avanzando

--- Ejercicio61/Ejercicio61/PErsona.cs
+++ Ejercicio61/Ejercicio61/PErsona.cs
@@ -12,6 +12,10 @@ namespace Ejercicio61
         private string nombre;
         private string apellido;
 
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+
         public Persona(int id, string nombre, string apellido)
             :this(nombre, apellido)
         {
@@ -24,5 +28,7 @@ namespace Ejercicio61
             this.nombre = nombre;
             this.apellido = apellido;
         }
+
+
     }
 }
--- Ejercicio61/Ejercicio61/PersonaDAO.cs
+++ Ejercicio61/Ejercicio61/PersonaDAO.cs
@@ -1,16 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace Ejercicio61
 {
     public class PersonaDAO
     {
-        public void Guardar()
+        SqlConnection connection = new SqlConnection("Data Source = CALIDAD\\SQLEXPRESS; Database = Persona; Trusted_Connection = true;");
+        public void Guardar(Persona persona)
         {
+            try
+            {
+                SqlCommand command = new SqlCommand();
+                command.Connection = connection;
+                command.CommandType = CommandType.Text;
+                command.CommandText = "INSERT INTO Datos VALUES(@ID, @Nombre, @Apellido)";
+                command.Parameters.Add(new SqlParameter("ID", persona.Id));
+                command.Parameters.Add(new SqlParameter("Nombre", persona.Nombre));
+                command.Parameters.Add(new SqlParameter("Apellido", persona.Apellido));
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.Open();
+                }
 
+                command.ExecuteNonQuery();
+            }
+            catch (ArgumentNullException ex)
+            {
+
+            }
+            catch (FormatException ex)
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        public SqlDataReader Leer(Persona persona)
+        {
+            SqlDataReader aux = null;
+            try
+            {
+                SqlCommand command = new SqlCommand();
+                command.Connection = connection;
+                command.CommandType = CommandType.Text;
+                command.CommandText = "SELECT * FROM Datos";
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.Open();
+                }
+
+                aux = command.ExecuteReader();
+            }
+            catch (ArgumentNullException ex)
+            {
+
+            }
+            catch (FormatException ex)
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return aux;
         }
     }
 }
