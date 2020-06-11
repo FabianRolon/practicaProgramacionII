64cc6973e69d4f870c4f981acc531058b1838855
FabianRolon <fabi18fr@gmail.com>
2020-06-11 11:39:51 -0300

    Ejecicio63

---
+++ Ejercicio63/Ejercicio63/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace Ejercicio63
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHora = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblHora
+            // 
+            this.lblHora.AutoSize = true;
+            this.lblHora.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHora.Location = new System.Drawing.Point(238, 110);
+            this.lblHora.Name = "lblHora";
+            this.lblHora.Size = new System.Drawing.Size(132, 45);
+            this.lblHora.TabIndex = 0;
+            this.lblHora.Text = "lblHora";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 285);
+            this.ControlBox = false;
+            this.Controls.Add(this.lblHora);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHora;
+    }
+}
+
---
+++ Ejercicio63/Ejercicio63/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio63
+{
+    public partial class Form1 : Form
+    {
+        Thread primerHilo;
+        Thread segundoHilo;
+        Random r1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            r1 = new Random();
+            ThreadStart ts = new ThreadStart(ModificarHora);
+            ThreadStart tsDos = new ThreadStart(ModificarColor);
+            primerHilo = new Thread(ts);
+            segundoHilo = new Thread(tsDos);
+            primerHilo.Start();
+            segundoHilo.Start();
+        }
+        delegate void DelegadoHora();
+        public void AgregarHora()
+        {
+            if (this.lblHora.InvokeRequired)
+            {
+                DelegadoHora d = new DelegadoHora(AgregarHora);
+                this.Invoke(d);
+            }
+            else
+            {
+                int c1 = r1.Next(0, 255);
+                int c2 = r1.Next(0, 255);
+                int c3 = r1.Next(0, 255);
+                int x = r1.Next(10, 340);
+                int y = r1.Next(10, 300);
+
+                lblHora.Text = DateTime.Now.ToString();
+                lblHora.ForeColor = Color.FromArgb(c1, c2, c3);
+                lblHora.Location = new Point(x, y);
+            }
+        }
+        delegate void DelegadoColor();
+        public void AgregarColor()
+        {
+            if (this.InvokeRequired)
+            {
+                DelegadoColor d = new DelegadoColor(AgregarColor);
+                this.Invoke(d);
+            }
+            else
+            {
+                int c1 = r1.Next(0, 255);
+                int c2 = r1.Next(0, 255);
+                int c3 = r1.Next(0, 255);
+
+                this.BackColor = Color.FromArgb(c1, c2, c3);
+            }
+        }
+        public void ModificarHora()
+        {
+            do
+            {
+                AgregarHora();
+                Thread.Sleep(1000);
+            } while (true);
+        }
+        public void ModificarColor()
+        {
+            do
+            {
+                AgregarColor();
+                Thread.Sleep(100);
+            } while (true);
+        }
+    }
+}
